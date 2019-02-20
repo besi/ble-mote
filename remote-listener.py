89b1031cf4da826cffb0b7e8971946243c2a7f0a
Beat Besmer <beat.besmer@gmail.com>
2019-02-20 15:30:58 +0100

    Publish the remote events (press down only) on MQTT

--- remote-listener.py
+++ remote-listener.py
@@ -1,8 +1,26 @@
+import json
+
 import evdev
+import paho.mqtt.publish as publish
 
 device = evdev.InputDevice('/dev/input/event1')
 print(device)
 
+down = 1
+up = 0
+hold = 2
+
 for event in device.read_loop():
-    if event.type == evdev.ecodes.EV_KEY:
-        print(evdev.categorize(event))
+    if event.type == evdev.ecodes.EV_KEY and event.value == down:
+        # print(evdev.categorize(event))
+        key = evdev.ecodes.KEY[event.code]
+        print(key)
+        payload = json.dumps({
+            "sec": event.sec,
+            "usec": event.usec,
+            "code": event.code,
+            "key": key,
+            "value": event.value
+        })
+
+        publish.single('devices/ble-remote/buttons/' + key, payload, hostname='192.168.1.112')
